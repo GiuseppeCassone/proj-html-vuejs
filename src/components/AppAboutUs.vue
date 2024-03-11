9c30172f1a5f4c7ba2b4a9644bc6e78fe5cf77d8
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-03-11 20:51:43 +0100

    correzione stilizzazione componente AppAboutUs

--- src/components/AppAboutUs.vue
+++ src/components/AppAboutUs.vue
@@ -7,18 +7,19 @@
 </script>
 
 <template>
-
-    <div class="container about-us">
-        <div class="row">
-            <div class="col-6"><img src="../../public/img/about.png" alt=""></div>
-            <div class="col-6 d-flex flex-column justify-content-center ps-5 gap-4">
-                <div class="about-logo">
-                    <img src="../../public/img/dog-icon.png" alt="">
-                    <span class="ps-3">About Us</span>
+    <div class="about-us">
+        <div class="container about-inner">
+            <div class="row">
+                <div class="col-6"><img src="../../public/img/about.png" alt=""></div>
+                <div class="col-6 d-flex flex-column justify-content-center ps-5 gap-4">
+                    <div class="about-logo">
+                        <img src="../../public/img/dog-icon.png" alt="">
+                        <span class="ps-3">About Us</span>
+                    </div>
+                    <h1>Welcome DogMilo Pets</h1>
+                    <p class="fs-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga esse excepturi soluta dolorem. Laborum, earum architecto dolor magnam aliquam temporibus corrupti ex excepturi labore at est consectetur, vitae, cum repellat?</p>
+                    <p class="fs-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi, magni vitae repudiandae aliquid provident nostrum beatae nesciunt repellat quis iusto reprehenderit rem! Asperiores earum deleniti aperiam similique vel. Natus, doloremque?</p>
                 </div>
-                <h1>Welcome DogMilo Pets</h1>
-                <p class="fs-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga esse excepturi soluta dolorem. Laborum, earum architecto dolor magnam aliquam temporibus corrupti ex excepturi labore at est consectetur, vitae, cum repellat?</p>
-                <p class="fs-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi, magni vitae repudiandae aliquid provident nostrum beatae nesciunt repellat quis iusto reprehenderit rem! Asperiores earum deleniti aperiam similique vel. Natus, doloremque?</p>
             </div>
         </div>
     </div>
@@ -28,8 +29,11 @@
 <style lang="scss">
 
     .about-us{
-        padding: 90px;
         background-color: white;
+
+        .about-inner{
+            padding: 90px 20px;
+        }
     }
 
 </style>
